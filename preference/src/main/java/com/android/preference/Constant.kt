1555d03dca80886d6cbefb01ff9b5fa02b238631
jankey <18311271399@163.com>
2023-03-28 15:27:13 +0800

    Auto stash before rebase of "refs/heads/jsd"

--- preference/src/main/java/com/android/preference/Constant.kt
+++ preference/src/main/java/com/android/preference/Constant.kt
@@ -1,8 +1,30 @@
 package com.android.preference
 
 /**
+<<<<<<< Updated upstream
  * @author: jiangshide 
  * @date: 2023/3/28
  * @email: 18311271399@163.com
  * @description: 
  */
+=======
+ * @author: jiangshide
+ * @date: 2023/3/28
+ * @email: 18311271399@163.com
+ * @description:
+ */
+
+const val ERROR = -1
+
+const val INTERVAL_TIME = 60*1000
+
+const val PING_PRIORITY = 1
+const val SOCKET_PRIORITY = 2
+const val HTTP_PRIORITY = 3
+
+const val ACTIVITY = false;
+
+const val PING_TIME_OUT = 800
+const val SOCKET_TIME_OUT = 500
+const val SOCKET_DEFAULT_PORT = 80
+>>>>>>> Stashed changes
--- preference/src/main/java/com/android/preference/IPPreference.kt
+++ preference/src/main/java/com/android/preference/IPPreference.kt
@@ -1,9 +1,19 @@
 package com.android.preference
 
 /**
+<<<<<<< Updated upstream
  * @author: jiangshide 
  * @date: 2023/3/28
  * @email: 18311271399@163.com
  * @description: 
  */object IPPreference {
+=======
+ * @author: jiangshide
+ * @date: 2023/3/28
+ * @email: 18311271399@163.com
+ * @description:
+ */
+object IPPreference {
+    
+>>>>>>> Stashed changes
 }
