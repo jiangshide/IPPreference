f88501aa7835cc53993fddc34ff4f4c438df5e4d
jankey <18311271399@163.com>
2023-03-28 15:29:04 +0800

    解决冲突

--- preference/src/main/java/com/android/preference/Constant.kt
+++ preference/src/main/java/com/android/preference/Constant.kt
@@ -7,12 +7,6 @@ package com.android.preference
  * @email: 18311271399@163.com
  * @description: 
  */
-=======
- * @author: jiangshide
- * @date: 2023/3/28
- * @email: 18311271399@163.com
- * @description:
- */
 
 const val ERROR = -1
 
@@ -27,4 +21,3 @@ const val ACTIVITY = false;
 const val PING_TIME_OUT = 800
 const val SOCKET_TIME_OUT = 500
 const val SOCKET_DEFAULT_PORT = 80
->>>>>>> Stashed changes
--- preference/src/main/java/com/android/preference/IPPreference.kt
+++ preference/src/main/java/com/android/preference/IPPreference.kt
@@ -1,19 +1,11 @@
 package com.android.preference
 
 /**
-<<<<<<< Updated upstream
- * @author: jiangshide 
- * @date: 2023/3/28
- * @email: 18311271399@163.com
- * @description: 
- */object IPPreference {
-=======
  * @author: jiangshide
  * @date: 2023/3/28
  * @email: 18311271399@163.com
- * @description:
+ * @description: 
  */
 object IPPreference {
     
->>>>>>> Stashed changes
 }
